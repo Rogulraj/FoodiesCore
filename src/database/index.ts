a510c8c81d275d6f1e429965200685dd76cda64b
Rogulraj <rogulraj@gamil.com>
2025-07-18 10:34:14 +0530

    fix: mongo cluster url change

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,7 +3,7 @@ import { NODE_ENV, DB_USERNAME, DB_DATABASE, DB_PASSWORD } from '@config';
 
 export const dbConnection = async () => {
   const dbConfig = {
-    url: `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster1.cdmff6z.mongodb.net/${DB_DATABASE}?retryWrites=true&w=majority&appName=Cluster1`,
+    url: `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.jx9a1cw.mongodb.net/${DB_DATABASE}?retryWrites=true&w=majority&appName=Cluster0`,
   };
 
   if (NODE_ENV !== 'production') {
