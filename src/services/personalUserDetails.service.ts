61bbfd546cfff10b9cf04bdc35b44f2e11c9a515
Rogulraj <rogulraj@gmail.com>
2025-08-18 00:33:29 +0530

    change: migrated AWS usage to supabase

--- src/services/personalUserDetails.service.ts
+++ src/services/personalUserDetails.service.ts
@@ -1,58 +1,70 @@
-import { AWS_MAIN_BUCKET } from '@/config';
 import { CommonResponse, IdResponse } from '@/interfaces/commonResponse.interface';
 import { PersonalUserDetails } from '@/interfaces/personalUserDetails';
 import { PersonalUserDetailsModel } from '@/models/personalUserDetails.model';
-import { AwsS3 } from '@/utils/s3';
+import appConfig from '@/config';
+import { supabaseClient } from '@/utils/supabase';
+import { HttpException } from '@/exceptions/httpException';
 
 export class PersonalUserDetailsService {
-  public async createUserDetails(userData: PersonalUserDetails): Promise<CommonResponse<IdResponse>> {
-    const base64Image: string = userData.imageUrl;
-    const imagePath: string = `users/image-${Date.now()}.jpeg`;
-    if (base64Image !== undefined && base64Image !== 'null') {
-      const s3 = new AwsS3();
-      await s3.uploadBase64Image(AWS_MAIN_BUCKET, imagePath, base64Image);
-      userData.imageUrl = imagePath;
+  public async createUserDetails(
+    userData: PersonalUserDetails
+  ): Promise<CommonResponse<IdResponse>> {
+    try {
+      const createData = await PersonalUserDetailsModel.create(userData);
+    
+      return {
+        statusCode: 201,
+        data: { _id: createData._id },
+        message: 'user details created',
+      };
+    } catch (error) {
+      throw new HttpException(500, 'createUserDetails: ' + error);
     }
-
-    const createData = await PersonalUserDetailsModel.create(userData);
-
-    return { statusCode: 201, data: { _id: createData._id }, message: 'user details created' };
   }
 
   public async getUserDetailsById(userId: string): Promise<CommonResponse<PersonalUserDetails>> {
     const userDetails = await PersonalUserDetailsModel.findById(userId);
-
-    const s3 = new AwsS3();
-    const signedUrl = await s3.getPreSignedUrl(AWS_MAIN_BUCKET, userDetails.imageUrl, 3600);
-    userDetails.imageUrl = signedUrl;
-
-    const data: CommonResponse<PersonalUserDetails> = { statusCode: 200, data: userDetails, message: 'user details fetched' };
-    return data;
-  }
-
-  public async updateUserDetailsById(userId: string, userData: PersonalUserDetails): Promise<CommonResponse<IdResponse>> {
-    const userDetails = await PersonalUserDetailsModel.findById(userId);
-
-    const base64Image = userData.imageUrl;
-    const imageUrl = userDetails.imageUrl;
-    if (base64Image !== undefined && base64Image !== 'null') {
-      const s3 = new AwsS3();
-      await s3.deleteObject(AWS_MAIN_BUCKET, imageUrl);
-
-      const imagePath = `restaurants/image-${Date.now()}.jpeg`;
-      await s3.uploadBase64Image(AWS_MAIN_BUCKET, imagePath, base64Image);
-
-      console.log(true);
-
-      userData.imageUrl = imagePath;
-    } else {
-      userData.imageUrl = imageUrl;
+    if (!userDetails) {
+      return {
+        statusCode: 404,
+        data: null,
+        message: 'user not found',
+      };
     }
 
-    Object.assign(userDetails, userData);
-    await userDetails.save();
+    const response: CommonResponse<PersonalUserDetails> = {
+      statusCode: 200,
+      data: userDetails,
+      message: 'user details fetched',
+    };
 
-    const response: CommonResponse<IdResponse> = { statusCode: 200, data: userDetails._id, message: 'user details updated' };
     return response;
   }
+
+  public async updateUserDetailsById(
+    userId: string,
+    userData: PersonalUserDetails
+  ): Promise<CommonResponse<IdResponse>> {
+    const userDetails = await PersonalUserDetailsModel.findById(userId);
+  
+    if (!userDetails) {
+      return {
+        statusCode: 404,
+        data: null,
+        message: 'user not found',
+      };
+    }
+  
+    // merge and save
+    Object.assign(userDetails, userData);
+    await userDetails.save();
+  
+    const response: CommonResponse<IdResponse> = {
+      statusCode: 200,
+      data: userDetails._id,
+      message: 'user details updated',
+    };
+  
+    return response;
+  }  
 }
--- src/services/restaurant.service.ts
+++ src/services/restaurant.service.ts
@@ -1,20 +1,11 @@
-import { AWS_MAIN_BUCKET } from '@/config';
 import { HttpException } from '@/exceptions/httpException';
 import { RequestWithId } from '@/interfaces/auth.interface';
 import { CommonResponse, IdNameResponse, IdResponse } from '@/interfaces/commonResponse.interface';
 import { AddMenuBody, MenuCategoryItems, MenuType, RestaurantType } from '@/interfaces/restaurant.interface';
 import { RestaurantModel } from '@/models/restaurant.model';
-import { AwsS3 } from '@/utils/s3';
-import { setHeapSnapshotNearHeapLimit } from 'v8';
 
 export class RestaurantService {
   public async createRestaurant(userData: RestaurantType): Promise<CommonResponse<RestaurantType>> {
-    const base64Image: string = userData.imageUrl;
-    const imagePath: string = `restaurants/image-${Date.now()}.jpeg`;
-
-    const s3 = new AwsS3();
-    await s3.uploadBase64Image(AWS_MAIN_BUCKET, imagePath, base64Image);
-    userData.imageUrl = imagePath;
 
     const createData = await RestaurantModel.create(userData);
 
@@ -25,17 +16,7 @@ export class RestaurantService {
   public async getAllRestaurants(): Promise<CommonResponse<RestaurantType[]>> {
     const findOne = await RestaurantModel.find();
 
-    const s3 = new AwsS3();
-
-    const addImageUrl = await Promise.all(
-      findOne.map(async item => {
-        const imageUrl = await s3.getPreSignedUrl(AWS_MAIN_BUCKET, item.imageUrl, 3600);
-        item.imageUrl = imageUrl;
-        return item;
-      }),
-    );
-
-    const data: RestaurantType[] = addImageUrl;
+    const data: RestaurantType[] = findOne;
 
     const responseData: CommonResponse<RestaurantType[]> = { statusCode: 200, data: data, message: 'restaurant data' };
     return responseData;
@@ -44,45 +25,12 @@ export class RestaurantService {
   public async getRestaurantById(restaurantId: string): Promise<CommonResponse<RestaurantType>> {
     const findOne = await RestaurantModel.findOne({ _id: restaurantId });
 
-    const s3 = new AwsS3();
-
-    const restaurantImageUrl = findOne.imageUrl;
-    const signedUrl = await s3.getPreSignedUrl(AWS_MAIN_BUCKET, restaurantImageUrl, 3600);
-    findOne.imageUrl = signedUrl;
-
-    for (const loop1 in findOne?.menu) {
-      const foods = findOne?.menu[loop1].items;
-
-      for (const loop2 in foods) {
-        const foodImageUrl = foods[loop2].imageUrl;
-
-        const signedUrl = await s3.getPreSignedUrl(AWS_MAIN_BUCKET, foodImageUrl, 3600);
-        foods[loop2].imageUrl = signedUrl;
-      }
-    }
-
     const responseData: CommonResponse<RestaurantType> = { statusCode: 200, data: findOne, message: 'restaurant data' };
     return responseData;
   }
 
   public async updateRestaurantDetails(restaurantId: string, userData: Partial<RestaurantType>): Promise<CommonResponse<IdResponse>> {
     const restaurant = await RestaurantModel.findById(restaurantId);
-
-    const base64Image = userData.imageUrl;
-    const imageUrl = restaurant.imageUrl;
-    if (base64Image !== undefined && base64Image !== 'null') {
-      const s3 = new AwsS3();
-      await s3.deleteObject(AWS_MAIN_BUCKET, imageUrl);
-
-      const imagePath = `restaurants/image-${Date.now()}.jpeg`;
-      await s3.uploadBase64Image(AWS_MAIN_BUCKET, imagePath, base64Image);
-
-      console.log(true);
-
-      userData.imageUrl = imagePath;
-    } else {
-      userData.imageUrl = imageUrl;
-    }
 
     Object.assign(restaurant, userData);
     await restaurant.save();
@@ -95,7 +43,6 @@ export class RestaurantService {
     const findData = await RestaurantModel.findOne({ _id: userId });
     const category = userData.category;
 
-    console.log(findData);
     const menuItem = findData.menu.find(item => item.category === category);
 
     if (menuItem) throw new HttpException(409, `This category ${userData.category} already exists`);
@@ -117,13 +64,6 @@ export class RestaurantService {
     const findData = await RestaurantModel.findOne({ _id: userId });
     if (!findData) throw new HttpException(409, 'This restaurant not exists');
 
-    const base64Image: string = userData.item.imageUrl;
-    const imagePath: string = `restaurants/image-${Date.now()}.jpeg`;
-
-    const s3 = new AwsS3();
-    s3.uploadBase64Image(AWS_MAIN_BUCKET, imagePath, base64Image);
-    userData.item.imageUrl = imagePath;
-
     const category = userData.category;
     const item = userData.item;
 
@@ -139,18 +79,6 @@ export class RestaurantService {
   public async getAllMenuItems(restaurantId: string): Promise<CommonResponse<MenuType[]>> {
     const findRestaurant = await RestaurantModel.findById(restaurantId);
     if (!findRestaurant) throw new HttpException(409, 'This restaurant does not exists');
-
-    const s3 = new AwsS3();
-
-    for (const i in findRestaurant?.menu) {
-      const menuItem = findRestaurant?.menu[i];
-      for (const x in menuItem.items) {
-        const imageUrl = menuItem?.items[x].imageUrl;
-
-        const signedUrl = await s3.getPreSignedUrl(AWS_MAIN_BUCKET, imageUrl, 3600);
-        menuItem.items[x].imageUrl = signedUrl;
-      }
-    }
 
     const menuData: MenuType[] = findRestaurant.menu;
 
@@ -168,11 +96,6 @@ export class RestaurantService {
 
     const foodData = findFoodData.menu[categoryIndex].items.find(food => food._id.toString() === foodId);
     if (!foodData) throw new HttpException(409, 'Food Does not exists');
-
-    const s3 = new AwsS3();
-    const imageUrl = foodData.imageUrl;
-    const signedUrl = await s3.getPreSignedUrl(AWS_MAIN_BUCKET, imageUrl, 3600);
-    foodData.imageUrl = signedUrl;
 
     const data: MenuCategoryItems = foodData;
 
@@ -194,20 +117,6 @@ export class RestaurantService {
 
     const foodIndex = category.items.findIndex(food => food._id.toString() === foodId);
     if (foodIndex === -1) throw new HttpException(409, 'Food does not exists');
-
-    if (item.imageUrl !== 'null') {
-      const s3 = new AwsS3();
-      const imageUrl = category.items[foodIndex].imageUrl;
-      await s3.deleteObject(AWS_MAIN_BUCKET, imageUrl);
-
-      const base64Image = item.imageUrl;
-      const imagePath = `restaurants/image-${Date.now()}.jpeg`;
-      await s3.uploadBase64Image(AWS_MAIN_BUCKET, imagePath, base64Image);
-
-      item.imageUrl = imagePath;
-    } else {
-      item.imageUrl = category.items[foodIndex].imageUrl;
-    }
 
     const foodData = category.items[foodIndex];
     category.items[foodIndex] = { ...foodData, ...item };
